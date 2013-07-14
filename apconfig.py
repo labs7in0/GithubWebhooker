1df92f554efb515611982afd84de0b5bbaf59dbf
Kinosang <me@kinosang.ws>
2013-07-14 15:56:05 +0800

    Update Codes

---
+++ apconfig.py
@@ -0,0 +1,9 @@
+repoList = {
+#    'Test1' : '~/Test1',
+#    'Test2' : '~/Test1'
+}
+
+refSet = {
+#    'Test1' : u'refs/heads/dev',
+#    'Test2' : u'refs/heads/dev'
+}
--- autopuller.py
+++ autopuller.py
@@ -11,6 +11,7 @@ def worker(repo, submittime, committer):
     output.write("* Git returned: \n")
     output.writelines(tmp)
     output.write("Repo: " + repo)
+    output.write("\nref: " + refSet[repo])
     output.write("\nDir: " + repoList[repo])
     output.write("\n- Submitted by " + committer + " at " + submittime + "\n")
     output.close()
@@ -34,11 +35,12 @@ class service:
         nowthetime = time.strftime("[%Y-%m-%d %H:%M:%S]",time.localtime(time.time()))
         para = web.input()
         value = json.loads(para.payload)
+        repoName = value[u'repository'][u'name']
         #if value[u'ref'] == u'refs/heads/master':
-        if value[u'ref'] == u'refs/heads/dev':
+        if value[u'ref'] == refSet[repoName]:
             output = open('./autopuller.log', 'a')
             output.write("\n" + nowthetime + "\n")
-            output.write("Repo: " + value[u'repository'][u'name'])
+            output.write("Repo: " + repoName)
             output.write("\n* Added: \n")
             output.writelines(value[u'head_commit'][u'added'])
             output.write("\n* Modified: \n")
@@ -47,7 +49,7 @@ class service:
             output.writelines(value[u'head_commit'][u'removed'])
             output.write("\n- Committer " + value[u'head_commit'][u'committer'][u'email'] + "\n")
             output.close()
-            th = threading.Thread(target=worker,args=(value[u'repository'][u'name'], nowthetime, value[u'head_commit'][u'committer'][u'email']))
+            th = threading.Thread(target=worker,args=(repoName, nowthetime, value[u'head_commit'][u'committer'][u'email']))
             th.start()
             return "Succeed"
         return "Parameter Wrong"
